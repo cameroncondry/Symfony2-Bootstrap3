059eddb490146fd7cd9789eb83f1f2766ed74ae1
Cameron Condry <ccondry@pltw.org>
2014-03-29 23:28:53 -0400

    Updated the base template and removed the no longer used services.yml file.

--- src/CBC/Bundle/BootstrapBundle/DependencyInjection/BootstrapExtension.php
+++ src/CBC/Bundle/BootstrapBundle/DependencyInjection/BootstrapExtension.php
@@ -22,9 +22,6 @@ class BootstrapExtension extends Extension implements PrependExtensionInterface
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
     }
 
     /**
